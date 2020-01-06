995b3a596c1d5c23a376f8b5a7cc1048894d77c4
Your Name <you@example.com>
2020-01-06 08:03:27 +0100

    tempsave

--- jpg/EN/index.php
+++ jpg/EN/index.php
@@ -19,7 +19,7 @@ font.monospace
 </style>
 <!-- "thank you" "dear" 1960s paedos for activating CCS Suicide Death Bot against me-->
 </head>
-<body onhashchange="myFunction();">
+<body onhashchange="myFunction();" style="background-color:#E6E6FA">
 <script>
 function myFunction() {
 	var allCards = document.getElementsByClassName("card");
